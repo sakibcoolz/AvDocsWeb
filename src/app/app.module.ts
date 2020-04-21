2377f8c2a9c107415bd271f5ec62f8eec9a6e617
Sakib Mulla <mr.sakibmulla@gmail.com>
2020-04-21 17:42:23 +0530

    OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { FullLayoutComponent } from './layout/full-layout/full-layout.component';
+import { SharedModule } from './shared/shared.module';
+import { AuthService } from './shared/auth/auth.service';
+import { AuthGuard } from './shared/auth/auth-guard.service';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CustomHttpInterceptor } from './shared/auth/customHttpInterceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentLayoutComponent,
+    FullLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
+  exports: [SharedModule],
+  providers: [
+    AuthService,
+    AuthGuard,
+    {
+      provide: LocationStrategy, useClass: HashLocationStrategy
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CustomHttpInterceptor,
+      multi: true
+   }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/routes/content-layout.routes.ts
@@ -0,0 +1,11 @@
+import { AuthGuard } from './../auth/auth-guard.service';
+import { Routes, RouterModule } from '@angular/router';
+
+//Route for content layout with sidebar, navbar and footer
+export const CONTENT_ROUTES: Routes = [
+  {
+    path: 'login', loadChildren: () => import('../../pages/login/login.module').then(m => m.LoginModule)
+  }
+];
+
+
