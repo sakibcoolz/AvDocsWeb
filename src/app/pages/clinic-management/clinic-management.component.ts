8ab46fae859c85e29be60ac3a3f48f9664c653aa
Sakib Mulla <mr.sakibmulla@gmail.com>
2020-04-26 00:39:01 +0530

    commit

--- src/app/pages/clinic-management/clinic-management.component.ts
+++ src/app/pages/clinic-management/clinic-management.component.ts
@@ -8,26 +8,40 @@ import { validateVerticalPosition } from '@angular/cdk/overlay';
   styleUrls: ['./clinic-management.component.css']
 })
 export class ClinicManagementComponent implements OnInit {
-  
+
   regularForm: FormGroup;
+  contactList: FormArray;
+
   constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.regularForm = this._fb.group({
-      'clinicName':['', [Validators.required, Validators.minLength(5)]],
-      'cityname':['', [Validators.required, Validators.minLength(3)]],
-      'address':['', Validators.required],
-      'pincode':['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
-      'district':['', Validators.required],
-      'contactNo':this._fb.array([this.addContact()]),
-      'country':['India', [Validators.required, Validators.minLength(4)]],
-      'createdBy':[0]
+      clinicName:['', [Validators.required, Validators.minLength(5)]],
+      cityname:['', [Validators.required, Validators.minLength(3)]],
+      address:['', Validators.required],
+      pincode:['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
+      district:['', Validators.required],
+      contactNo:this._fb.array([this.addContact()]),
+      country:['India', [Validators.required, Validators.minLength(4)]],
+      createdBy:[0]
     })
+
+    this.contactList = this.regularForm.get('contactNo') as FormArray;
+  }
+
+  getContactsFormGroup(index): FormGroup {
+    // this.contactList = this.form.get('contacts') as FormArray;
+    const formGroup = this.contactList.controls[index] as FormGroup;
+    return formGroup;
+  }
+
+  get contactFormGroup() {
+    return this.regularForm.get('contactNo') as FormArray;
   }
 
   addContact():FormGroup {
     return this._fb.group({
-      'telenumber':['', [Validators.required,Validators.minLength(10), Validators.maxLength(14)]]
+      telenumber:['', [Validators.required,Validators.minLength(10), Validators.maxLength(14)]]
     })
   }
 
