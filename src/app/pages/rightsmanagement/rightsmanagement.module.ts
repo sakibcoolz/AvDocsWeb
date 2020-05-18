e890df11949357a928160e05475c72d8dae0de48
Sakib Mulla <mr.sakibmulla@gmail.com>
2020-05-19 00:21:38 +0530

    commit

--- src/app/pages/rightsmanagement/rightsmanagement.module.ts
+++ src/app/pages/rightsmanagement/rightsmanagement.module.ts
@@ -4,7 +4,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatPaginatorModule} from '@angular/material/paginator';
 import { RightsmanagementRoutingModule } from './rightsmanagement-routing.module';
 import { RightsmanagementComponent } from './rightsmanagement.component';
 import { AddRightsComponent } from './add-rights/add-rights.component';
@@ -13,6 +13,9 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {MatTableModule} from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { ViewRightsComponent } from './view-rights/view-rights.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSortModule} from '@angular/material/sort';
+
 
 @NgModule({
   declarations: [RightsmanagementComponent, AddRightsComponent, EditRightsComponent, ViewRightsComponent],
@@ -26,7 +29,10 @@ import { ViewRightsComponent } from './view-rights/view-rights.component';
     MatFormFieldModule,
     MatCheckboxModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule
   ]
 })
 export class RightsmanagementModule { }
--- src/app/pages/rightsmanagement/view-rights/view-rights.component.ts
+++ src/app/pages/rightsmanagement/view-rights/view-rights.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { clinicright } from 'src/app/shared/model/token.model';
 import { jwtToken } from 'src/app/shared/auth/jwtRelate';
 import { RightsService } from 'src/app/shared/service/rights.service';
+import {HttpClient} from '@angular/common/http';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-view-rights',
@@ -10,16 +14,36 @@ import { RightsService } from 'src/app/shared/service/rights.service';
 })
 export class ViewRightsComponent implements OnInit {
 
+  add: boolean;
+  edit: boolean;
+  delete: boolean;
+  view: boolean;
   clinicid: number;
-  clinicright : clinicright[];
+  // clinicright : clinicright[];
+  displayedColumns: string[] = ['ID', 'rightsname', 'Edit', 'Delete'];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  dataSource = new MatTableDataSource<clinicright>();
   constructor(private _rightsService : RightsService) {
     this.clinicid = jwtToken.getClinicId()
+    this.add = jwtToken.addright('rights', true);
+    this.edit = jwtToken.editright('rights', true);
+    this.delete = jwtToken.deleteright('rights', true);
   }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     this._rightsService.getrightbyid(this.clinicid).subscribe(data => {
-      this.clinicright = data;
+      this.dataSource.data = data;
     })
   }
 
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
+
+
